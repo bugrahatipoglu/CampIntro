62758134050aa6d306c1cc4ad158065c318668be
bugrahatipoglu <bugrahatipogluu@gmail.com>
2021-03-08 23:13:32 +0300

    20210308_campwork_finish

---
+++ CampIntro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            //do not repeat yourself
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmiMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmiMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+
+            }
+            else
+            {
+                Console.WriteLine("Giris Yap Butonu");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            //type-safe -- tip güvenli
+            foreach (Urun urun in urunler) //Urun yerine var da yazılabilir.
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("----------");
+
+            }
+
+            Console.WriteLine("---------Metotlar--------");
+            //instance - örnek
+            //encapsulation - kapsülleme - düzenli yapı oluşturma
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+            //sepetManager.Ekle();
+            //sepetManager.Ekle();
+            //sepetManager.Ekle();
+
+            sepetManager.Ekle2("Armut", "Yesil Armut", 12,10);
+            sepetManager.Ekle2("Elma", "Yesil Elma", 16,9);
+            sepetManager.Ekle2("Karpuz", "Beyaz Karpuz", 19,8);
+
+        }
+    }
+}
+
+
+// Do not repeat yourself - DRY - Clean Code - Best Practice 
