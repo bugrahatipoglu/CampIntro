8bd810ee4c1e15c9c12299e8d8465e0bc4b6749f
bugrahatipoglu <bugrahatipogluu@gmail.com>
2021-03-12 20:54:06 +0300

    20210312 class_5

---
+++ OOP2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "12345678910";
+
+
+            //Kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            // Gerçek müşteri - Tüzel müşteri farklı şeylerdir.
+            //SOLID - L 
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekle(musteri1);
+            customerManager.Ekle(musteri2);
+
+
+
+        }
+    }
+}
---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    //Birbirlerinin alternatifi olan ama içerikleri farklı olan durumlarda interface kullanılır.
+    interface IKrediManager
+    {
+        void Hesapla();
+        void BiseyYap();
+        
+    }
+}
